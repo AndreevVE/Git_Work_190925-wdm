835b0c0ad7659902c8576decf7ea574519187770
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-01-06 12:08:51 +0100

    Автокоммит: 2026-01-06 12:08:51

--- copyfiles.sh
+++
@@ -1,9 +0,0 @@
-for file in "$source_directory"/*."$file_extension"; do
-    filename=$(basename "$file")
-    name_without_ext="${filename%.*}"
-
-    cp "$file" "$target_directory/$name_without_ext.$new_file_extension"
-
-    echo "Скопирован файл: $filename -> $name_without_ext.$new_file_extension"
-done
-
